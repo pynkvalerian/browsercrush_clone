51d020feca60a3d76230a6be6c7195de22763077
pynkvalerian <pynkvalerian@gmail.com>
2019-07-31 12:33:41 +0800

    updated app with malaysian flags

--- public/application.js
+++ public/application.js
@@ -1,59 +1,70 @@
-  function randomGenerator() {
-    return Math.random() * (800 - 1) + 1;
+function randomGenerator(direction) {
+  if(direction == 'width') {
+    return Math.floor(Math.random() * (window.screen.width + 1));
+  } else {
+    return Math.floor(Math.random() * (window.screen.height + 1)) + 80;
   }
+}
 
-  function popUpBrowser() {
-    var categories = ['buildings', 'food', 'nature', 'people', 'technology', 'objects']
-    var randCategory = categories[Math.floor(Math.random() * categories.length)];
+function popUpFlag() {
+  $('.container').append("<div class='flag'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/1949_Malaya_Flag_Proposal_3.svg/300px-1949_Malaya_Flag_Proposal_3.svg.png' width='100px'></div>");
+  $('.flag').last().css('position', 'absolute').css('top', randomGenerator('height') + 'px').css('right', randomGenerator('width') + 'px');
+}
 
-    $('.container').append("<div class='browser'><div class='panel panel-default'><button class='btn btn-danger pull-right'>x</button><div class='panel-body'><img src='https://source.unsplash.com/category/"+randCategory+"/400x300'></div></div></div>");
-    $('.browser').last().css('position', 'absolute').css('top', randomGenerator() + 'px').css('right', randomGenerator() + 'px');
-  }
+function gameOver(finalScore) {
+  $('body').css('position', 'static');
+  $('.end').css('z-index', '10');
+  $('.end').css('background-color', 'rgba(250,250,250,0.9)');
+  $('h2#final-score').text(finalScore);
+  $('.final-score').show();
+}
 
-  function gameOver(finalScore) {
-    $('.end').css('z-index', '10');
-    $('.end').css('background-color', 'rgba(250,250,250,0.8)');
-    $('.final-score p').html('Your Score: ' + finalScore);
-    $('.final-score').show();
-  }
+function moveBody() {
+  $('body').css('position', 'relative');
+  var direction = ['top', 'bottom', 'left', 'right']
+  setInterval(function(){
+    var randDirection = direction[Math.floor(Math.random() * direction.length)];
+    var randNumber = Math.random()*10
+    $('body').css(randDirection, randNumber);
+  },10)
+}
 
 $(document).ready(function(){
   var clickCounter = 0
-  var lives = 3
+  var timer = 15
 
   // hide welcome message on start
   $('#start').on('click', function(){
     $('.intro').hide();
 
+    // start countdown timer
+    var countDown = setInterval(function(){
+      timer = timer - 1
+      $('h2#timer').text(timer);
+
+      if (timer === 0) {
+        clearInterval(countDown)
+        gameOver(clickCounter);
+      } else if (timer < 5) {
+        $('.timer').css('color', 'red')
+        moveBody()
+      }
+    },1000)
+
     setInterval(function(){
-      popUpBrowser();
-    }, 500);
-
+      popUpFlag();
+    }, 200);
   })
 
-  // removes browser if click on close button
-  $('body').on('click', '.btn-danger', function(){
-    $(this).parent().parent().remove();
+  // removes flag if click on flag
+  $('body').on('click', '.flag', function(){
+    $(this).remove();
     clickCounter += 1;
-    $('#score').text('Score: ' + clickCounter);
-  })
-
-  // minus score if click on panel body
-  $('body').on('click', '.panel-body', function(){
-    lives -= 1;
-    $('span.glyphicon#' + lives).attr('class', 'glyphicon glyphicon-heart-empty');
-    clickCounter -= 3;
-    $('#score').text('Score: ' + clickCounter);
-
-    // if lives is 0, game over!
-    if(lives === 0){
-      gameOver(clickCounter);
-    }
+    $('#score').text(clickCounter);
   })
 
   // restart game
   $('body').on('click', '#restart', function(){
     location.reload();
   })
-
 });
