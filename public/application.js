2f62e48227b55434c62545b316e44b9471e31fdd
pynkvalerian <pynkvalerian@gmail.com>
2019-08-08 14:12:46 +0800

    added us flag and email form

--- public/application.js
+++ public/application.js
@@ -1,3 +1,6 @@
+let malaysiaFlag;
+let foreignFlags;
+
 function randomGenerator(direction) {
   if(direction == 'width') {
     return Math.floor(Math.random() * (window.screen.width + 1));
@@ -16,7 +19,8 @@ function popUpForeignFlag() {
     "",
     "flags/indonesia.svg",
     "flags/singapore.svg",
-    "flags/thailand.svg"
+    "flags/thailand.svg",
+    "flags/us.svg"
   ]
   randomInt = Math.floor(Math.random() * (foreignFlags.length - 1)) + 1;
 
@@ -25,8 +29,8 @@ function popUpForeignFlag() {
 }
 
 function shareButtonDimensions(id) {
-  $(id).css('height', '100px');
-  $(id).css('width', '100px');
+  $(id).css('height', '50px');
+  $(id).css('width', '50px');
 }
 
 function gameOver(finalScore) {
@@ -42,43 +46,61 @@ function gameOver(finalScore) {
 
 function moveBody() {
   $('body').css('position', 'relative');
-  var direction = ['top', 'bottom', 'left', 'right']
+  let direction = ['top', 'bottom', 'left', 'right']
   setInterval(function(){
-    var randDirection = direction[Math.floor(Math.random() * direction.length)];
-    var randNumber = Math.random()*10
+    let randDirection = direction[Math.floor(Math.random() * direction.length)];
+    let randNumber = Math.random()*10
     $('body').css(randDirection, randNumber);
   },10)
 }
 
+function submitScore(score) {
+  let name = $('input[name="name"]').val()
+  let email = $('input[name="email"]').val()
+  let url = "https://hooks.zapier.com/hooks/catch/3349809/ob6kacc/?name=" + name + "&email=" + email + "&score=" + score
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: function(result) {
+      $('form').css('display', 'none');
+      $('form').css('display', 'block');
+    },
+    error: function(result) {
+    }
+  })
+}
+
 $(document).ready(function(){
-  var clickCounter = 0
-  var timer = 15
+  let clickCounter = 0
+  let timer = 15
 
   // hide welcome message on start
   $('#start').on('click', function(){
     $('.intro').hide();
 
+    let malaysiaFlag = setInterval(function(){
+      popUpFlag();
+    }, 200);
+
+    let foreignFlags = setInterval(function(){
+      popUpForeignFlag();
+    }, 400);
+
     // start countdown timer
-    var countDown = setInterval(function(){
+    let countDown = setInterval(function(){
       timer = timer - 1
       $('h2#timer').text(timer);
 
       if (timer === 0) {
         clearInterval(countDown)
         gameOver(clickCounter);
+        clearInterval(malaysiaFlag)
+        clearInterval(foreignFlags)
       } else if (timer < 5) {
         $('.timer').css('color', 'red')
         moveBody()
       }
     },1000)
-
-    setInterval(function(){
-      popUpFlag();
-    }, 200);
-
-    setInterval(function(){
-      popUpForeignFlag();
-    }, 400);
   })
 
   // removes flag + add score if click on malaysian flag
@@ -98,5 +120,11 @@ $(document).ready(function(){
   // restart game
   $('body').on('click', '#restart', function(){
     location.reload();
+  })
+
+  // when submit form
+  $('.form').on('submit', function(e){
+    e.preventDefault();
+    submitScore(clickCounter)
   })
 });
